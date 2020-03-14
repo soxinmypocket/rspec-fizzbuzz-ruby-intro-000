9a7b86ee8b2d7d4600794956a9e95bbd20d2cc31
mathematical-integer-7084 <shawnaejohnson6@gmail.com>
2020-03-14 05:59:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if (num % 5 == 0) && (num % 3 == 0) 
+  if (num % 5 == 0) && (num % 3 == 0)
     return "FizzBuzz"
